20ba20b481651ba7ecdf1454504d9d1edb140fd9
Chevanlol360 <chevanlol360@CQ-ABA-HP.(none)>
2014-04-19 10:25:34 -0400

    Fast Charge Support & Frandom Support

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -50,6 +50,12 @@
 #endif
 #include <mach/rpm-regulator.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#define USB_FASTCHG_LOAD 1000 /* uA */
+#endif 
+
+
 #ifdef CONFIG_BU52031NVX
 #include <linux/mfd/pm8xxx/cradle.h>
 #endif
@@ -1169,6 +1175,14 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 
 	if (motg->cur_power == mA)
 		return;
+#ifdef CONFIG_FORCE_FAST_CHARGE
+   if (force_fast_charge == 1) {
+       mA = USB_FASTCHG_LOAD;
+      pr_info("USB fast charging is ON - 1000mA.\n");
+   } else {
+     pr_info("USB fast charging is OFF.\n");
+   }
+#endif
 
 	dev_info(motg->phy.dev, "Avail curr from USB = %u\n", mA);
 /* Update battery status after setting charger online state
